7e11782baf0dc1f595d0234b45edf56c69a97330
block-hac <cn.corestar@gmail.com>
2022-08-12 02:58:00 +0800

    divid contracts and add crypto kitty interface

--- contracts/ZombieFactory.sol
+++ contracts/ZombieFactory.sol
@@ -14,9 +14,16 @@ contract ZombieFactory {
 
     Zombie[] public zombies;
 
-    function _createZombie(string memory _name, uint256 _dna) private {
+    mapping (uint256 => address) public zombieToOwner;
+    mapping (address => uint256) ownerZombieCount;
+
+    function _createZombie(string memory _name, uint256 _dna) internal {
         zombies.push(Zombie(_name, _dna));
-        uint id = zombies.length;
+        uint256 id = zombies.length - 1;
+
+        zombieToOwner[id] = msg.sender;
+        ownerZombieCount[msg.sender]++;
+        
         emit NewZombie(id, _name, _dna);
     }
 
@@ -30,6 +37,7 @@ contract ZombieFactory {
     }
 
     function createRandomZombie(string memory _name) public {
+        require(ownerZombieCount[msg.sender] == 0);
         uint256 randDna = _generateRandomDna(_name);
         _createZombie(_name, randDna);
     }
